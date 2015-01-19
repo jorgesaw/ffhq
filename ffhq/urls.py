173ff4c158fa0d764170c42afa6185c04b022b90
Emiliano Dalla Verde Marcozzi <edvm@planisys.com>
2015-01-19 20:01:16 -0300

    improvments at login form, added about page

--- ffhq/urls.py
+++ ffhq/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from ffhq import views
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^about/', views.about, name='about'),
     url(r'^accounts/', include('allauth.urls')),
 )

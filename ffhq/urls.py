c51d41444aaba0da1b69d8e51e82aa8e38f6e7f4
Emiliano Dalla Verde Marcozzi <edvm@planisys.com>
2015-01-19 22:22:29 -0300

    enhancements at login and logout

--- ffhq/urls.py
+++ ffhq/urls.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from ffhq import views
 
 urlpatterns = patterns('',
+    url(r'^$', views.home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^about/', views.about, name='about'),
+    url(r'^dashboard/', views.dashboard, name='dashboard'),
     url(r'^accounts/', include('allauth.urls')),
 )
--- ffhq/views.py
+++ ffhq/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+
+
+def home(request):
+    return redirect('dashboard')
 
 
 def about(request):
     return render(request, 'about.html')
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html')
